581693cd1672ddddda203a7480edddcd0c7b94d1
wwnss00 <wns208500@gmail.com>
2025-08-26 21:02:28 +0900

    상품 리포지토리 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter 
+@Getter
 public abstract class Item {
 
     @Id
