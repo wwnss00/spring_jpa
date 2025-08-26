e6c9c95f0617302f5786a5faad24066ebcdbeaa6
wwnss00 <wns208500@gmail.com>
2025-08-26 19:20:35 +0900

    회원기능 테스트

---
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+
+}
