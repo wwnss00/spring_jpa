68cd84ac5dad8b3a9f1df208d4bb3eb30d9df7ad
wwnss00 <wns208500@gmail.com>
2025-08-27 18:08:37 +0900

    주문 서비스 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -35,6 +35,9 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
 
+    protected Order() {
+    }
+
     //==연관관계 메서드==//
 
     public void setMember(Member member) {
